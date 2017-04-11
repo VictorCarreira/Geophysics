0bb3e624d4028c71ca2b04c49a9a5835dd1519cd
VictorCarreira <carreiravr@gmail.com>
2017-04-11 20:48:41 -0300

    Atualiza o programa para a versão Primitive.f95

---
+++ Primitive.f95
@@ -0,0 +1,43 @@
+program calculos                                                                !inicia o programa
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!Definindo o tipo de variávis!!!!!!!!!!!!!!!!!!!!!!!!
+implicit none                                                                   !Não especifica nenhum tipo de variável
+complex k                                                                       !Diz que k é uma variável complexa
+integer n, m, raio                                                              !Diz que n, m, raio, E e D são variáveis inteiras
+real pi, r, area, E, D, x, y                                                    !Diz que pi, x, y, r, area são uma variáveis reais
+!!!!!!!!!!!!!Atribuindo valores a variáveis e definindo parâmetros!!!!!!!!!!!!!!
+parameter(raio=3)                                                               !Define um nome simbólico para uma constante e não pode ser modificado
+parameter(pi=3.14159)
+n=1                                                                             !Atribui o valor 1 a variável n
+m=0                                                                             !Atribui o valor 3 a variável m
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Criando funções!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!k=m+n+pi                                                                       !Define a vairável k como uma somatória
+!print*, 'k = ', k                                                              !Imprime o valor de k
+
+!write (*,*) 'Dado o raio r:'                                                   !Escreve no terminal o valor de r
+!read (*,*) r                                                                   !lê o valor de r a partir do terminal
+!area=4*pi*r*r                                                                  !Cálculo da área de uma esfera
+!write (*,*) 'Área da esfera=', area
+
+!write (*,*) 'Dado o número:'
+!read (*,*) E
+!D=E**(1./2.)                                                                   !Calcula a raíz quadrada. Para o FORTRAN considerar o resto das divisões é necessário acrescentar o ponto depois de cada valor numérico na expressão
+!write (*,*) 'Sua raíz quadrada =', D
+
+!write (*,*) 'x='
+!read (*,*) x
+!y=sin(x)
+!write (*,*) 'sen(x)=', y
+
+
+write (*,*) 'm='
+read (*,*) m
+if(m .ne. 0)then                                                                !If expandido
+  k=m+n
+  print*,'k=m+n=',k
+elseif (m .eq. 0)then
+  k=(m+n)**2
+  print*, 'k=(m+n)²=',k
+endif
+
+end program calculos                                                            !Termina o programa
