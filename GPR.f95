e9ab0ea8513694f2b755d94c9ffbbb6b2f5b5655
VictorCarreira <victorcarreira@on.br>
2019-07-05 17:32:15 -0300

    Adicona o MODULE gpr

---
+++ GPR.f95
@@ -0,0 +1,32 @@
+MODULE gpr
+!--------------------------------------------------------------------!
+!This module should contain all main subroutines related to ground   !
+!penetrating radar                                                   !
+!Author: Victor Carreira                                             !
+!--------------------------------------------------------------------!
+IMPLICIT NONE
+  PUBLIC
+  INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=4)
+  INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(8,10)
+  INTEGER(KIND=SP):: 
+  INTEGER(KIND=SP), ALLOCATABLE, DIMENSION(:)::
+  REAL(KIND=DP)::
+  REAL(KIND=DP), ALLOCATABLE, DIMENSION(:)::
+  REAL(KIND=DP), ALLOCATABLE, DIMENSION(:,:)::
+ 
+CONTAINS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+END MODULE gpr
